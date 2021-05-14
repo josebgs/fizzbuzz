0f7875549aaf413df03c937def5126106805bcb8
josebgs <jsantos@riis.com>
2021-05-14 11:03:37 -0400

    added user input

--- fizzbuzz.kt
+++ fizzbuzz.kt
@@ -1,12 +1,16 @@
 fun main(){
-    var cnt = 0
-    do{
-      when{
-        (cnt%15) == 0 -> println("fizzbuzz")
-        (cnt%3) == 0 -> println("fizz")
-        (cnt%5) == 0 -> println("buzz")
+
+  var cnt = readLine()?.toInt()
+
+  while (cnt != 0){
+
+    when{
+        (cnt?.rem(15)) == 0 -> println("fizzbuzz")
+        (cnt?.rem(3)) == 0 -> println("fizz")
+        (cnt?.rem(5)) == 0 -> println("buzz")
         else -> println("$cnt")
       }
-      cnt++
-    }while(cnt <100)
+      cnt = readLine()?.toInt()
+  }
+
 }
